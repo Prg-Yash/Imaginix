31811be55065da05c73058bc0266e4549e522d81
Prg-Yash <127442680+Prg-Yash@users.noreply.github.com>
2024-06-02 23:32:53 +0530

    fixes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,7 @@
-// import { User } from "lucide-react";
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
       <p>Home</p>
-      {/* <UserButton afterSignOutUrl="/" /> */}
     </div>
   );
 }
--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,26 +1,32 @@
-import mangoose, {Mongoose} from 'mongoose';
+import mongoose, { Mongoose } from 'mongoose';
 
+const MONGODB_URL = process.env.MONGODB_URL;
 
-const MONGODB_URL= process.env.MONGODB_URL ;
-
-interface MoongooseConnection {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
-let cached : MoongooseConnection = (global as any).mangoose;
+let cached: MongooseConnection = (global as any).mongoose
+
+if(!cached) {
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
+}
+
 export const connectToDatabase = async () => {
-if(!cached){
-    cached = (global as any).mangoose = {conn: null, promise: null};
-}
-if (!MONGODB_URL){
-    throw new Error('Please define the MONGODB_URL environment variable inside .env.local');
-}
-cached.promise = cached.promise||mangoose.connect(MONGODB_URL, {
-    dbName:'imaginix',bufferCommands: false,
-});
+  if(cached.conn) return cached.conn;
 
-cached.conn = await cached.promise;
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
-return  cached.conn;
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'imaginix', bufferCommands: false 
+    })
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 }
