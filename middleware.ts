8b41f192664180e392bc10f9bec38ed735ec622a
Prg-Yash <127442680+Prg-Yash@users.noreply.github.com>
2024-07-08 01:30:03 +0530

    changes

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,4 @@
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// const isProtectedRoute = createRouteMatcher(["/", "/credits(.*)"]);
-
-// export default clerkMiddleware((auth, req) => {
-//   if (isProtectedRoute(req)) {
-//     auth().protect();
-//   }
-// });
-
-// export const config = {
-//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
 
 import { authMiddleware } from "@clerk/nextjs/server";
 
