5594d7bfd89feda014025fb44f3d825d6ccd1f6f
Prg-Yash <127442680+Prg-Yash@users.noreply.github.com>
2024-07-08 02:06:41 +0530

    refactor: update authMiddleware import in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,12 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
-export default authMiddleware({
-  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
-});
- 
-export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+  // import { authMiddleware } from "@clerk/nextjs";
+
+  import { authMiddleware } from "@clerk/nextjs";
+
+  
+  export default authMiddleware({
+    publicRoutes: [ '/api/webhooks/clerk', '/api/webhooks/stripe']
+  });
+  
+  export const config = {
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  };
