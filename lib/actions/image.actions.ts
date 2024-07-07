7676190c3caabad0eabb7ceaeedccda9815d2304
Prg-Yash <127442680+Prg-Yash@users.noreply.github.com>
2024-07-08 01:05:33 +0530

    completed

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -10,7 +10,7 @@ const populateUser = (query: any) =>
   query.populate({
     path: "author",
     model: "User",
-    select: "_id firstName lastName",
+    select: "_id firstName lastName clerkId",
   });
 
   export async function addImage({ image, userId, path }: AddImageParams) {
@@ -118,3 +118,32 @@ export async function getAllImages({limit=9,page=1,searchQuery=""}:{
   }
 }
 
+export async function getUserImages({
+  limit = 9,
+  page = 1,
+  userId,
+}: {
+  limit?: number;
+  page: number;
+  userId: string;
+}) {
+  try {
+    await connectToDatabase();
+
+    const skipAmount = (Number(page) - 1) * limit;
+
+    const images = await populateUser(Image.find({ author: userId }))
+      .sort({ updatedAt: -1 })
+      .skip(skipAmount)
+      .limit(limit);
+
+    const totalImages = await Image.find({ author: userId }).countDocuments();
+
+    return {
+      data: JSON.parse(JSON.stringify(images)),
+      totalPages: Math.ceil(totalImages / limit),
+    };
+  } catch (error) {
+    handleError(error);
+  }
+}
